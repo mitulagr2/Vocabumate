85aa961a649d465b75c03aea11c26ce69bcba6f5
mitulagr2 <mitulagr2@gmail.com>
2024-03-02 12:21:45 +0530

    setup work manager

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,8 @@ dependencies {
 
   implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
   implementation("androidx.navigation:navigation-compose:2.7.7")
+  implementation("androidx.work:work-runtime-ktx:2.9.0")
+
   // Retrofit
   implementation("com.squareup.retrofit2:retrofit:2.9.0")
   // Retrofit with Scalar Converter
--- app/src/main/java/com/example/vocabumate/Constants.kt
+++ app/src/main/java/com/example/vocabumate/Constants.kt
@@ -12,4 +12,11 @@ val NOTIFICATION_TITLE: CharSequence = "WorkRequest Starting"
 const val CHANNEL_ID = "VERBOSE_NOTIFICATION"
 const val NOTIFICATION_ID = 1
 
-const val DELAY_TIME_MILLIS: Long = 3000
+// The name of the fetch word work
+const val FETCH_WORD_WORK_NAME = "fetch_word_work"
+
+// Other keys
+const val KEY_API_SERVICE = "KEY_API_SERVICE"
+const val KEY_WORD_QUERY = "KEY_WORD_QUERY"
+const val TAG_OUTPUT = "OUTPUT"
+const val KEY_QUERY_OUTPUT = "KEY_QUERY_OUTPUT"
